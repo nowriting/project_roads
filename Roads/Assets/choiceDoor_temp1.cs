5b87ee6e541741a5d757c23bb69b4174db1a9cf6
eliza <no.writing@gmail.com>
2017-05-01 22:30:24 +0300

    5 commits fix

---
+++ Roads/Assets/choiceDoor_temp1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class choiceDoor_temp1 : MonoBehaviour {
+
+    // Temp because when I figure out how to get the tags to cooperate on doors this will be obsolete;
+
+    public string scene_trek = "scene_trek";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTriggerEnter(Collider choiceDoor_temp1)
+    {
+        SceneManager.LoadScene(scene_trek);
+    }
+}
---
+++ Roads/Assets/choiceDoors.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class choiceDoors : MonoBehaviour {
+
+    public string scene_australia = "scene_australia";
+    // public string town_door1 = "Town_Choice1";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider choiceDoors)
+    {
+        // Damn thing shows as Untagged. Even when it's tagged. Temp solution.
+        // Debug.Log("Trigger name: " + choiceDoors.tag);
+
+        // if (choiceDoors.tag == "test")
+        // {
+            SceneManager.LoadScene(scene_australia);
+        // }
+    }
+}
