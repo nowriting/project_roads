a167a7a7182fc5ca99e766d484117a5ec272bba7
eliza <no.writing@gmail.com>
2017-05-16 17:57:06 +0300

    linear_interpolation

---
+++ Roads/Assets/LinearInterpolation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearInterpolation : MonoBehaviour {
+
+    [SerializeField]
+    private Transform startTransform;
+
+    [SerializeField]
+    private Transform endTransform;
+
+    [SerializeField]
+    private float movementTimeInSeconds = 5f;
+
+    private IEnumerator OnTriggerEnter(Collider boulder)
+    {
+        float t = 0f;
+
+        while (t < 1f)
+        {
+            transform.position = Vector3.Lerp(startTransform.position, endTransform.position, t);
+            t += Time.deltaTime / movementTimeInSeconds;
+            yield return null;
+        }
+
+    }
+}
